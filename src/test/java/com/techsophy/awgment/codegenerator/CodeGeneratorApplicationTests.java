fd7f58fb9f06a5dae111bf27e75a418d77b0a4e3
Sanjay Singh <sanjay.s@techsophy.com>
2023-07-12 11:11:22 +0530

    first commit

---
+++ src/test/java/com/techsophy/awgment/codegenerator/CodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsophy.awgment.codegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
